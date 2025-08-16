5d7613d5d7875a2918a968fb73f56db430e751f4
Darragh Nugent <darraghnugent312@gmail.com>
2025-08-16 21:27:12 +1000

    Added basic UI. Picture doesn't show up yet

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static("client"));
+
+
 app.use("/terrain", terrainRoutes);
 app.use("/user", userRoutes);
 app.use("/auth", authRoutes);
