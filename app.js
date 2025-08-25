30b99431262c77ac56568de50f963720fda9f0ba
Darragh Nugent <darraghnugent312@gmail.com>
2025-08-25 18:11:52 +1000

    Fixed HMTL issue

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static("client"));
-
+app.use(express.static(path.join(__dirname, "client")));
 
 app.use("/terrain", terrainRoutes);
 app.use("/user", userRoutes);
@@ -36,6 +35,6 @@ app.use("/rule", ruleRoutes);
 
 //app.use(errorHandler);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server listening on port ${port}`);
 });
