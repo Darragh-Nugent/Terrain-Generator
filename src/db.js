cf4b970ae8be12aff50db59542359a99c4be77b6
Darragh Nugent <darraghnugent312@gmail.com>
2025-09-24 16:26:34 +1000

    Changed PostgreDB users table to use UUIDs. Need to connect it to Cognito

--- src/db.js
+++ src/db.js
@@ -18,8 +18,13 @@ const pool = new Pool({
     console.log('PostgreSQL connection successful.');
 
     await client.query(`
+      DROP TABLE IF EXISTS terrains;
+      DROP TABLE IF EXISTS users;
+    `);
+
+    await client.query(`
       CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
+        id UUID PRIMARY KEY,
         username VARCHAR(100) UNIQUE NOT NULL,
         password VARCHAR(100) NOT NULL
       )
@@ -28,7 +33,7 @@ const pool = new Pool({
     await client.query(`
       CREATE TABLE IF NOT EXISTS terrains (
         id SERIAL PRIMARY KEY,
-        user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
         seed INT NOT NULL,
         size INT NOT NULL,
         heightScale INT NOT NULL,
