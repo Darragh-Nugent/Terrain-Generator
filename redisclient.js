84f5fa12269c4e08566f4b0294f94a94cc322580
HuanNguyen-Dev <hnguy138@outlook.com>
2025-09-25 19:29:06 +1000

    redis uses tls as transit encryption is enabled

--- redisclient.js
+++ redisclient.js
@@ -1,11 +1,16 @@
-// redisclient.js
 const { createClient } = require('redis');
 const REDIS_URL = process.env.REDIS_URL;
-const client = createClient({ url: `${REDIS_URL}` });
+
+const client = createClient({ 
+  url: `${REDIS_URL}`, 
+  socket: {
+    connectTimeout: 10000,  // Increase timeout to 10 seconds
+    tls: {}  // For encrypted connection if Transit Encryption is enabled
+  }
+});
+
 client.on('error', (err) => console.error('Redis error:', err));
 
 (async () => {
   await client.connect();
 })();
-
-module.exports = client;
